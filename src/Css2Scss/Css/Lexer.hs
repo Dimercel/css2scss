dce39ceb65af60968505be42cadc1719a144f2b8
Dimercel <xolcman@gmail.com>
2015-12-23 19:21:24 +0300

    Add tokens in lexer

--- src/Css2Scss/Css/Lexer.hs
+++ src/Css2Scss/Css/Lexer.hs
@@ -1,1 +1,13 @@
-module Css2Scss.Css.Lexer () where
+module Css2Scss.Css.Lexer
+    ( Token (..)
+    , h
+    ) where
+
+import Text.ParserCombinators.Parsec
+
+data Token = H String deriving (Eq, Show)
+
+h :: Parser Token
+h = do
+    hex <- many (oneOf "0123456789abcdef")
+    return $ H hex

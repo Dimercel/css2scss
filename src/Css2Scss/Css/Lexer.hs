8707a73d0e86d5610500f6271ef26faa003caccb
Dimercel <xolcman@gmail.com>
2015-12-27 21:50:54 +0300

    More tokens

--- src/Css2Scss/Css/Lexer.hs
+++ src/Css2Scss/Css/Lexer.hs
@@ -3,6 +3,9 @@ module Css2Scss.Css.Lexer
     , h
     , nonascii
     , unicode
+    , escape
+    , nmstart
+    , nmchar
     ) where
 
 import Text.ParserCombinators.Parsec
@@ -11,11 +14,12 @@ import Text.Parsec.Char
 data Token = H String | Nonascii String deriving (Eq, Show)
 
 h :: Parser Char
-h = hexDigit
+h = hexDigit <?> "h"
 
 nonascii :: Parser String
 nonascii = do
         count 1 (satisfy (\x -> x >= '\o240' && x <= '\o4177777'))
+        <?> "nonascii"
 
 unicode :: Parser String
 unicode = do
@@ -24,3 +28,27 @@ unicode = do
             <|> many1 h
         end <- try (count 1 (oneOf " \t\r\n\f"))
         return $ concat [initial, hex, end]
+        <?> "unicode"
+
+escape :: Parser String
+escape = do
+        try unicode
+        <|> do
+                initial <- count 1 (oneOf "\\")
+                symbol <- count 1 (oneOf $ ['\o240'..'\o4177777'] ++ "-~")
+                return $ concat [initial, symbol]
+        <?> "escape"
+
+nmstart :: Parser String
+nmstart = do
+        count 1 (oneOf ['a'..'z'])
+        <|> nonascii
+        <|> escape
+        <?> "nmstart"
+
+nmchar :: Parser String
+nmchar = do
+        count 1 (oneOf $ ['a'..'z'] ++ ['0'..'9'] ++ "-")
+        <|> nonascii
+        <|> escape
+        <?> "nmchar"

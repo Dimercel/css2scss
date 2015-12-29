b3108900d89d26fcc098ebd3074bd315b6a64fba
Dimercel <xolcman@gmail.com>
2015-12-29 22:13:37 +0300

    Negative test

--- test/Css2Scss/Test/Lexer.hs
+++ test/Css2Scss/Test/Lexer.hs
@@ -15,3 +15,8 @@ run = hspec $ do
             let predicate x = parse L.h "test" [x] == Right x
                 in take 22 (cycle [True]) == foldl' (\acc x ->
                     (predicate x) : acc) [] "0123456789abcdefABCDEF"
+
+        it "Test for wrong hex digits" $ do
+            case parse L.h "test" "g" of
+                Left _ -> True
+                Right _ -> False

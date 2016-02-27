c663b09a9086bec14187548c2a352131ede1defc
Dimercel <xolcman@gmail.com>
2016-02-27 18:21:31 +0300

    SCSS data types

--- src/Css2Scss/Scss.hs
+++ src/Css2Scss/Scss.hs
@@ -1,3 +1,11 @@
 module Css2Scss.Scss () where
 
+import Data.Tree
+
 import Css2Scss.Scss.Render
+import Css2Scss.Scss.Converter
+
+
+data Property = Property { name :: String, value :: String } deriving(Show, Eq)
+data Rule     = Rule { selector :: String, props :: [Property]} deriving(Show, Eq)
+type Ruleset  = Tree Rule

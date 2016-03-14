228b4c1031ac761c4d37d7ac33efc5505d7bd2ab
Dimercel <xolcman@gmail.com>
2016-03-14 21:49:14 +0300

    Test correction

--- test/Css2Scss/Test/Css.hs
+++ test/Css2Scss/Test/Css.hs
@@ -8,11 +8,25 @@ import Control.Applicative
 import Control.Monad
 
 listWithToken:: Token -> Gen [Token]
-listWithToken t = shuffle =<< (t :) <$> (arbitrary :: Gen [Token])
+listWithToken t = do
+        genList <- (t :) <$> (arbitrary :: Gen [Token])
+        result <- shuffle genList
+        return result
+
+listWithOutToken:: Token -> Gen [Token]
+listWithOutToken t = (filter (/= t)) <$> (arbitrary :: Gen [Token])
 
 testGetTokBefore t ts = C.getTokBefore t ts /= [] ==> t `notElem` ts
     ==> (length $ fst $ span (/=t) ts) == (length $ C.getTokBefore t ts)
 
+testGetTokAfter t ts
+    | t `elem` ts = t `elem` ts
+        ==> (length $ C.getTokAfter t ts) < length ts
+        ==> (head $ C.getTokAfter t ts) /= t
+    | t `notElem` ts = t `notElem` ts 
+        ==> C.getTokAfter t ts == []
+
 run :: IO ()
 run = do
         quickCheck (testGetTokBefore :: Token -> [Token] -> Property)
+        quickCheck (testGetTokAfter :: Token -> [Token] -> Property)

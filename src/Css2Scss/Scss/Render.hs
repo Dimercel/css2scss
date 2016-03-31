237d21883f39d36c2743cc380501ab93f0894d60
Dimercel <xolcman@gmail.com>
2016-03-31 15:38:38 +0300

    Render begins

--- src/Css2Scss/Scss/Render.hs
+++ src/Css2Scss/Scss/Render.hs
@@ -1,1 +1,24 @@
-module Css2Scss.Scss.Render () where
+module Css2Scss.Scss.Render (Renderer(..)) where
+
+
+import Data.List
+import Css2Scss.Scss
+
+eol :: String
+eol = "\n"
+
+indentSize :: Int
+indentSize = 4
+
+indent :: Int -> String
+indent size = replicate indentSize ' '
+
+class Renderer a where
+        render :: a -> String
+        renderWithIndent :: a -> Int -> String
+
+instance Renderer Property where
+        render (Property name val) = name ++ " : " ++ val ++ ";"
+        renderWithIndent prop level = indent level ++ (render prop)
+
+

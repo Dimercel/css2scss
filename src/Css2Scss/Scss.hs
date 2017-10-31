2e31c149a8d8ee89d52dc1254b451e77ff571272
Dimercel <xolcman@gmail.com>
2017-10-31 17:01:47 +0300

    Refinement in import for scss module

--- src/Css2Scss/Scss.hs
+++ src/Css2Scss/Scss.hs
@@ -13,13 +13,13 @@ import Data.Tree (Tree(..))
 import Data.Label
 import Test.QuickCheck (Arbitrary(..), arbitrary)
 
-import qualified Css2Scss.Css as C
+import qualified Css2Scss.Css as Css
 
 
 -- Scss-правило представляет собой, то же что
 -- и css-правило, но с поддержкой древовидной
 -- структуры
-type Rule     = Tree C.Rule
+type Rule     = Tree Css.Rule
 
 type Ruleset  = [Tree Rule]
 

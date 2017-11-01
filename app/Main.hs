7a110a8f0bc974b57f3ec5701bc0342ed345dc2a
Dimercel <xolcman@gmail.com>
2017-11-01 17:33:34 +0300

    Scss output

--- app/Main.hs
+++ app/Main.hs
@@ -3,21 +3,24 @@ module Main where
 import System.Environment
 import System.IO
 import Text.ParserCombinators.Parsec
-import Css2Scss.Css.Parser
-import Css2Scss.Css.Lexer
+import Css2Scss.Css.Parser ( preprocessor
+                           , stylesheet)
 import Css2Scss.Css as C
-import Css2Scss.Scss.Converter
+import Css2Scss.Scss.Render (PrettyRenderer(..))
+import Css2Scss.Scss.Converter (toScssRules)
 
 
 main :: IO ()
 main = do
-        args <- getArgs
-        handle <- openFile (head args) ReadMode
-        contents <- hGetContents handle
-        let tokens = do
-                parse stylesheet "" (preprocessor contents)
-        {-print test'-}
-        case tokens of
-            Right t -> print $ buildSCSSRulesets $ C.buildRulesets $ splitOnBaseLexems t
-            Left err -> print err
-        hClose handle
+  args <- getArgs
+  handle <- openFile (head args) ReadMode
+  contents <- hGetContents handle
+  let tokens = parse stylesheet "" (preprocessor contents)
+  case tokens of
+      Right t -> putStrLn $ concatMap renderPretty (toScssRules $ onlyRules t)
+      Left err -> print err
+  hClose handle
+  where
+    isRule (C.RuleItem _) = True
+    isRule _ = False
+    onlyRules x = map (\(C.RuleItem x) -> x) (filter isRule x)
